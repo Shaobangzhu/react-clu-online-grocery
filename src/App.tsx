7d3c1862298e8533c0fa315e33b05987f4d00fc2
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-14 22:02:22 -0700

    forwardRef + useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-// Hook 相关的类型定义
+// Hook 相关的类型定义, forwardRef, useImperativeHandle
 
-import { useEffect, useRef } from 'react';
+import { ReactNode, useImperativeHandle, useRef, forwardRef } from "react";
+
+type PropsType = {
+  children: ReactNode
+}
+
+type RefType = {
+  start: () => void;
+}
+
+const Child = forwardRef<RefType, PropsType>((props, ref) => {
+  useImperativeHandle(ref, () => {
+    return {
+      start() {
+        console.log('start');
+      }
+    }
+  })
+
+  return <div>{props.children}</div>
+})
 
 function App() {
-
-  const ref = useRef<number | null>(null);
-
-  useEffect(() => {
-    ref.current = window.setTimeout(() => {
-      console.log('timer')
-    }, 1000);
-    return clearTimeout(ref.current);
-  }, [])
+  const ref = useRef<RefType>(null!);
 
   return (
-    <div>Hello Ref Hook</div>
+    <>
+      <button onClick={() => {ref.current.start()}}>click</button>
+      <Child ref={ref}>React is the best!</Child>
+    </>
   );
 }
 
